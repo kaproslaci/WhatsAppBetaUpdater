0a7b3a6b89044dd40ecc3d8790ea967ac9351d2f
Zoltan Szilvai <zoltan@kiflyapp.com>
2017-12-19 18:35:58 +0100

    Configs modified

--- app/src/main/java/com/javiersantos/funtactiqbetaupdater/Config.java
+++ app/src/main/java/com/javiersantos/funtactiqbetaupdater/Config.java
@@ -6,7 +6,8 @@ public class Config {
     public static final String GITHUB_TAGS = GITHUB_URL.concat("/new");
     public static final String GITHUB_APK = GITHUB_URL.concat("/assets/releases/download/");
     public static final String FUNTACTIQ_URL = "http://fundater.azurewebsites.net";
-    public static final String FUNTACTIQ_APK = FUNTACTIQ_URL.concat("/assets/releases/download/v1.0.0/com.funtactiq.apk");
+    //public static final String FUNTACTIQ_APK = FUNTACTIQ_URL.concat("/assets/releases/download/v1.0.0/com.funtactiq.apk");
+    public static final String FUNTACTIQ_APK = "https://fundater.blob.core.windows.net/apks/com.funtactiq.apk";
     public static final String PAYPAL_DONATION = "donate@javiersantos.me";
 
     public static final String PATTERN_LATEST_VERSION = "<p class=\"version\" align=\"center\">Version";
